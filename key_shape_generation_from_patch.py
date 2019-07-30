d181c9560d21dbbaa044e3c71b96763d19994a58
KangCai <caikangpku@gmail.com>
2019-07-30 20:21:24 +0800

    Update key_shape_generation_from_patch.py

--- key_shape_generation_from_patch.py
+++ key_shape_generation_from_patch.py
@@ -9,12 +9,17 @@ import numpy as np
 from skimage.feature import daisy
 from sklearn.cluster import KMeans
 
+N = 1000000
+FEATURE_SAVE_NAME = 'features_total.txt'
+CLUSTER_CENTER_SAVE_NAME = 'cluster_centers.txt'
+K = 150
+
 def GenFeatures():
     PATCH_DIR = key_shape_generation_from_original.SAVE_PATCH_PATH
     file_list = os.listdir(PATCH_DIR)
     t_start = time.clock()
     data = []
-    for i in range(100000):
+    for i in range(N):
         try:
             (filename, extension) = os.path.splitext(file_list[i])
             if extension != '.png':
@@ -27,7 +32,7 @@ def GenFeatures():
             print(i, file_list[i], e)
         if i % 1000 == 0:
             print('%r files processed, %r valid features generated, %ds cost' % (i, len(data), int(time.clock() - t_start)))
-    save_file_path = os.path.join(key_shape_generation_from_original.SAVE_PATCH_CLUSTER_CENTER_PATH, 'features_total.txt')
+    save_file_path = os.path.join(key_shape_generation_from_original.SAVE_PATCH_CLUSTER_CENTER_PATH, FEATURE_SAVE_NAME)
     with open(save_file_path, 'w+') as f:
         f.write(str(data))
     # with open(save_file_path) as f:
@@ -35,16 +40,15 @@ def GenFeatures():
     #     print(len(eval(a)))
 
 def Cluster():
-    save_feature_file_path = os.path.join(key_shape_generation_from_original.SAVE_PATCH_CLUSTER_CENTER_PATH, 'features.txt')
+    save_feature_file_path = os.path.join(key_shape_generation_from_original.SAVE_PATCH_CLUSTER_CENTER_PATH, FEATURE_SAVE_NAME)
     with open(save_feature_file_path) as f:
         a = f.read()
         feature_list = np.array([i[0] for i in eval(a)])
         print(feature_list.shape)
         model_kmeans = kmeans.KmeansModel()
-        K = 150
-        save_center_file_path = os.path.join(key_shape_generation_from_original.SAVE_PATCH_CLUSTER_CENTER_PATH, 'cluster_centers.txt')
+        save_center_file_path = os.path.join(key_shape_generation_from_original.SAVE_PATCH_CLUSTER_CENTER_PATH, CLUSTER_CENTER_SAVE_NAME)
         cluster_res, centers = model_kmeans.cluster(feature_list, K, save_file=save_center_file_path)
         print(centers)
 
 if __name__ == '__main__':
-    Cluster()
+    GenFeatures()
